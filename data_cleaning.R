aedc203085274898db96d57e8228ddd10761c5a1
Pedro-Toledo <57258959+Pedro-Toledo@users.noreply.github.com>
2020-09-15 18:11:57 -0300

    data cleaning inicial

--- data_cleaning.R
+++ data_cleaning.R
@@ -3,13 +3,22 @@ library(tidyverse)
 library(lubridate)
 
 # Dados
-data <- read_file('data/_chat.txt')
+raw <- read_file('data/_chat.txt')
 
 # Limpeza de Dados
-data <- data %>% 
+data <- raw %>% 
   str_replace_all('\n', ' ') %>% 
   str_replace_all('\r', ' ') %>% 
   str_split('.(?=\\[[0-9]{2}\\/[0-9]{2}\\/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}\\])') %>% 
   setNames('mensagem') %>% 
   bind_rows() %>% 
-  mutate()
+  mutate(data = str_extract(mensagem,
+                            '\\[[0-9]{2}\\/[0-9]{2}\\/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}\\]'),
+         autor = str_extract(mensagem, 'Juju|Pedro Toledo'),
+         mensagem = str_extract(mensagem, '(?<=Juju:|Pedro Toledo:).+')) %>% 
+  mutate(across(everything(), ~str_trim(.x)),
+         hora = str_extract(data, '[0-9]{2}:[0-9]{2}:[0-9]{2}'),
+         data = dmy(str_extract(data, '[0-9]{2}\\/[0-9]{2}\\/[0-9]{4}'))) %>% 
+  select(autor, data, hora, mensagem)
+
+saveRDS(data, 'data/clean_data.RDS')
